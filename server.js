aa37aaf6d23dd5edb70e5372c1dc1b045c1a15ba
unknown <mmilner@m3lx6402.m3technologypartners.com>
2013-08-13 10:48:36 -0500

    Added html and image

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.write('<html><body><h1>The Node</h1>');
+    res.write('<img src="/TheNode.jpg" height="300px" />');
+    res.end('</body></html>');
     
 }).listen(process.env.PORT || 8080);
