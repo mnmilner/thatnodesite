f94e7490de79a6e8f7c19e44730aabb7d0d9cf46
unknown <mmilner@m3lx6402.m3technologypartners.com>
2013-08-13 15:15:31 -0500

    fixed dependency

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body><h1>The Node</h1>');
+    res.write('<html><body><h1>The Node Stache</h1>');
     res.write('<img src="/TheNode.jpg" height="300px" />');
     res.end('</body></html>');
     
